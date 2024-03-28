e1fd3d14e0d56ff5a2adf17919116b522171e05b
lacterra <159001405+lacterra@users.noreply.github.com>
2024-03-28 08:34:39 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import re
+from typing import List, Set
+
+import Day1
+import Day2
+import Day4
+import Day5
+import Day6
+
+#Day1.Program()
+#Day2.Program()  
+#Day4.Program()
+#Day5.Program()
+Day6.Program()
